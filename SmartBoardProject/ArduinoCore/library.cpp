534f0dd61b57cb397ca3fad890f133c5ff212fcb
OfficialNSA <chromsuport@gmail.com>
2021-05-31 12:51:31 +0200

    Initial commit Creation of new/empty atmel soution with stepper and servo included

---
+++ SmartBoardProject/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 31.05.2021 12:48:58
+ * Author : Josua
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
